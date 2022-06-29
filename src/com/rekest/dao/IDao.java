7e03610aceaae8649a965f9797246e13749dc017
EspoirNorbert <espoirnorbert@gmail.com>
2022-06-29 04:06:01 +0200

    Integration de l'interface DAO

--- src/com/rekest/dao/IDao.java
+++ src/com/rekest/dao/IDao.java
@@ -1,4 +1,62 @@
 package com.rekest.dao;
 
+import java.util.List;
 
-public interface IDao {}
+import com.rekest.exeptions.DAOException;
+
+public interface IDao {
+	
+	/**
+	 * @param entity
+	 * @throws Exception 
+	 * @ 
+	 */
+	public void save(Object entity) throws DAOException ;
+
+	/**
+	 * @param entity
+	 * @throws Exception 
+	 * @ 
+	 */
+	public void delete(Object entity) throws DAOException ;
+
+	/**
+	 * @param entityClass
+	 * @param primaryKey
+	 * @return
+	 * @throws Exception 
+	 * @ 
+	 */
+	public Object find(Object entityClass, Integer primaryKey) throws DAOException ;
+	
+	/**
+	 * @param entityClass
+	 * @return
+	 * @ 
+	 */
+	public List<Object> list(Object entityClass) throws DAOException;
+	
+	/**
+	 * @param entityClass
+	 * @return
+	 * @throws Exception 
+	 * @ 
+	 */
+	public List<Object> list(Class<?> entityClass, String whereClause) throws DAOException ;
+	
+	/**
+	 * @param entity
+	 * @ 
+	 */
+	public void update(Object entity) throws DAOException;
+
+	/**
+	 * @param entityClass
+	 * @param whereClause
+	 * @return
+	 * @throws Exception 
+	 * @ 
+	 */
+	public Object find(Class<?> entityClass, String whereClause) throws DAOException ;
+	
+}
