6b295b6f463f10573c8bab2d7d1ca6d2f0ff5d4e
ccbobs13 <ccb.obs13@gmail.com>
2022-07-10 18:11:35 +0000

    Ajout des getters et setters

--- application/src/com/rekest/entities/Departement.java
+++ application/src/com/rekest/entities/Departement.java
@@ -45,5 +45,38 @@ public class Departement {
 	public static void copy(Departement oldDepartement, Departement newDepartment) {
 		oldDepartement.setNom(newDepartment.getNom());
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public StringProperty getSpdNom() {
+		return spdNom;
+	}
+
+	public void setSpdNom(StringProperty spdNom) {
+		this.spdNom = spdNom;
+	}
+
+	public List<Service> getServices() {
+		return services;
+	}
+
+	public void setServices(List<Service> services) {
+		this.services = services;
+	}
+	
 	
 }
