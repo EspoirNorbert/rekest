5a931d25cc10e5858ab77988f249d56d66f399b5
ccbobs13 <ccb.obs13@gmail.com>
2022-07-19 06:29:44 +0000

    Complete all for create and update employe

--- application/src/com/rekest/controllers/EmployeEditDialogController.java
+++ application/src/com/rekest/controllers/EmployeEditDialogController.java
@@ -1,16 +1,16 @@
 package com.rekest.controllers;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import com.rekest.entities.Service;
 import com.rekest.entities.employes.Employe;
-import com.rekest.exeptions.DAOException;
 import com.rekest.feature.IFeature;
 import com.rekest.feature.impl.Feature;
+import com.rekest.utils.Utilitaire;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -32,7 +32,7 @@ public class EmployeEditDialogController {
 	private Circle circlePhoto;
 
 	@FXML
-	private ComboBox<?> comboBoxProfil;
+	private ComboBox<String> comboBoxProfil;
 
 	@FXML
 	private ComboBox<String> comboBoxService;
@@ -55,7 +55,9 @@ public class EmployeEditDialogController {
 	@FXML
 	private TextField textFieldTelephone;
 
-	private List<Service> servicesList ;
+	private ObservableList<Service> servicesList ;
+
+	private List<String> profilesList = Utilitaire.getProfiles();
 
 	private Stage dialogStage;
 
@@ -70,147 +72,190 @@ public class EmployeEditDialogController {
 	 * Sets the stage of this dialog.
 	 * @param dialogStage
 	 */
-	 public void setDialogStage(Stage dialogStage) {
-		 this.dialogStage = dialogStage;
-	 }
+	public void setDialogStage(Stage dialogStage) {
+		this.dialogStage = dialogStage;
+	}
 
-	 /**
-	  * Sets the employe to be edited in the dialog.
-	  *
-	  * @param employe
-	  */
-	 public void setEmploye(Employe employe) {
-		 this.employe = employe;
-		 textFieldNom.setText(employe.getNom());
-		 textFieldPrenom.setText(employe.getPrenom());
-		 textFieldAdresse.setText(employe.getAdresse());
-		 textFieldEmail.setText(employe.getEmail());
-		 textFieldTelephone.setText(employe.getTelephone());
-		 //		comboBoxService.setItems(feature.OLService.getData());
-	 }
+	/**
+	 * Sets the employe to be edited in the dialog.
+	 *
+	 * @param employe
+	 */
+	public void setEmploye(Employe employe) {
+		this.employe = employe;
+		textFieldNom.setText(employe.getNom());
+		textFieldPrenom.setText(employe.getPrenom());
+		textFieldAdresse.setText(employe.getAdresse());
+		textFieldEmail.setText(employe.getEmail());
+		textFieldTelephone.setText(employe.getTelephone());
+		this.employe.setOldService(employe.getService());
+	}
 
-	 public Employe getEmploye() {
+	public Employe getEmploye() {
 		return employe;
 	}
 
 	/**
-	  * Initialize the value of combox box service
-	  * 
-	  */
-	 public void setServices() throws DAOException{
+	 * Initialize the value of combox box service
+	 * 
+	 */
+	public void setServices(){
 
-		 this.setServicesList(feature.listerServices());
+		this.setServicesList(feature.loadServicesObservableList());
 
-		 comboBoxService.setItems(FXCollections.observableArrayList(serialize(getServicesList())));
+		comboBoxService.setItems(FXCollections.observableArrayList(serialize(getServicesList())));
 
-	 }
+		if(this.employe!=null && this.employe.getServiceString()!=null) 
+			comboBoxService.setValue(this.employe.getServiceString());
 
-	 /**
-	  * Returns true if the employe clicked OK, false otherwise.
-	  *
-	  * @return
-	  */
-	 public boolean isOkClicked() {
-		 return okClicked;
-	 }
+	}
 
-	 /**
-	  * Called when the employe clicks ok.
-	  */
-	 @FXML
-	 private void handleClickedOk() {
-		 if (isInputValid()) {
-			 employe.setNom(textFieldNom.getText());
-			 employe.setPrenom(textFieldPrenom.getText());
-			 employe.setAdresse(textFieldAdresse.getText());
-			 employe.setEmail(textFieldEmail.getText());
-			 employe.setTelephone(textFieldTelephone.getText());
-			 
-			 okClicked = true;
-			 dialogStage.close();
-		 }
-	 }
+	/**
+	 * Initialize the value of combox box profil
+	 * 
+	 */
+	public void setProfiles(){
 
-	 /**
-	  * Called when the user clicks cancel.
-	  */
-	 @FXML
-	 private void handleClickedAnnuler() {
-		 dialogStage.close();
-		 reset();
-	 }
+		comboBoxProfil.setItems(FXCollections.observableArrayList(getProfilesList()));
 
-	 /**
-	  * Validates the user input in the text fields.
-	  *
-	  * @return true if the input is valid
-	  */
-	 private boolean isInputValid() {
-		 String errorMessage = "";
+		if(this.employe!=null && this.employe.getEmployeProfil()!=null) 
+			comboBoxProfil.setValue(this.employe.getEmployeProfil());
 
-		 if (textFieldNom.getText() == null || textFieldNom.getText().length() == 0) {
-			 errorMessage += "Le nom de l'employe est invalide!\n";
-		 }
+	}
 
-		 if (textFieldPrenom.getText() == null || textFieldPrenom.getText().length() == 0) {
-			 errorMessage += "Le prenom de l'employe est invalide!\n";
-		 }
+	/**
+	 * Returns true if the employe clicked OK, false otherwise.
+	 *
+	 * @return
+	 */
+	public boolean isOkClicked() {
+		return okClicked;
+	}
 
-		 if (textFieldEmail.getText() == null || textFieldEmail.getText().length() == 0) {
-			 errorMessage += "L'email de l'employe est invalide!\n";
-		 }
+	/**
+	 * Called when the employe clicks ok.
+	 */
+	@FXML
+	private void handleClickedOk() {
+		if (isInputValid()) {
+			employe.setNom(textFieldNom.getText());
+			employe.setPrenom(textFieldPrenom.getText());
+			employe.setAdresse(textFieldAdresse.getText());
+			employe.setEmail(textFieldEmail.getText());
+			employe.setTelephone(textFieldTelephone.getText());
+			employe.setService(getCurrentComboBoxService());
+			
+			okClicked = true;
+			dialogStage.close();
+		}
+	}
 
-		 if (textFieldAdresse.getText() == null || textFieldAdresse.getText().length() == 0) {
-			 errorMessage += "L'adresse de l'employe est invalide!\n";
-		 }
+	/**
+	 * Called when the user clicks cancel.
+	 */
+	@FXML
+	private void handleClickedAnnuler() {
+		reset();
+		dialogStage.close();
+	}
 
-		 if (textFieldTelephone.getText() == null || textFieldTelephone.getText().length() == 0) {
-			 errorMessage += "Le telephone de l'employe est invalide!\n";
-		 }
+	/**
+	 * Validates the user input in the text fields.
+	 *
+	 * @return true if the input is valid
+	 */
+	private boolean isInputValid() {
+		String errorMessage = "";
+
+		if (textFieldNom.getText() == null || textFieldNom.getText().length() == 0) {
+			errorMessage += "Le nom de l'employe est invalide!\n";
+		}
+
+		if (textFieldPrenom.getText() == null || textFieldPrenom.getText().length() == 0) {
+			errorMessage += "Le prenom de l'employe est invalide!\n";
+		}
+
+		if (textFieldEmail.getText() == null || textFieldEmail.getText().length() == 0) {
+			errorMessage += "L'email de l'employe est invalide!\n";
+		}
+
+		if (textFieldAdresse.getText() == null || textFieldAdresse.getText().length() == 0) {
+			errorMessage += "L'adresse de l'employe est invalide!\n";
+		}
+
+		if (textFieldTelephone.getText() == null || textFieldTelephone.getText().length() == 0) {
+			errorMessage += "Le telephone de l'employe est invalide!\n";
+		}
+		
+		if (comboBoxService.getValue() == null || comboBoxService.getValue().length() == 0) {
+			errorMessage += "Le service de l'employe est invalide!\n";
+		}
 
 
-		 if (errorMessage.length() == 0) {
-			 return true;
-		 } else {
-			 // Show the error message.
-			 com.rekest.utils.Utilitaire.alert(AlertType.WARNING, 
-					 dialogStage, 
-					 "Invalid Fields", 
-					 "Please correct invalid fields", 
-					 errorMessage);
+		if (errorMessage.length() == 0) {
+			return true;
+		} else {
+			// Show the error message.
+			com.rekest.utils.Utilitaire.alert(AlertType.WARNING, 
+					dialogStage, 
+					"Invalid Fields", 
+					"Please correct invalid fields", 
+					errorMessage);
 
-			 return false;
-		 }
-	 }
+			return false;
+		}
+	}
 
-	 public List<Service> getServicesList() {
-		 return servicesList;
-	 }
+	public ObservableList<Service> getServicesList() {
+		return servicesList;
+	}
 
-	 public void setServicesList(List<Service> servicesList) {
-		 this.servicesList = servicesList;
-	 }
+	public void setServicesList(ObservableList<Service> servicesList) {
+		this.servicesList = servicesList;
+	}
 
-	 /**
-	  * Serialize a list of services 
-	  * @param service list
-	  * @return list of strings with service id and name only
-	  */
-	 public List<String> serialize(List<Service> serviceList){
-		 
-		 List<String> strings = new ArrayList<>();
+	public List<String> getProfilesList() {
+		return profilesList;
+	}
 
-		 for (Iterator<Service> iterator = serviceList.iterator(); iterator.hasNext();) {
-			 Service service = (Service) iterator.next();
-			 strings.add(service.getId() + " - " + service.getNom());
-		 }
-		 
-		 return strings;
-	 }
+	/**
+	 * Serialize a observablelist of services 
+	 * @param service list
+	 * @return list of strings with service id and name only
+	 */
+	public List<String> serialize(ObservableList<Service> serviceList){
 
-	 public void reset() {
-		 this.servicesList = null;
-		 comboBoxService.setItems(null);
-	 }
+		List<String> strings = new ArrayList<>();
+
+
+		for (Service service : serviceList) {
+			strings.add(service.getId() + " - " + service.getNom());
+		}
+
+		return strings;
+	}
+	
+	public Service getCurrentComboBoxService() {
+		Service tempService = null ;
+		for (Service service : getServicesList()) {
+			if ((service.getId() + " - " + service.getNom()).equals(comboBoxService.getValue()))
+				tempService =  service;
+		}
+		return tempService;
+	}
+
+	public void reset() {
+		this.servicesList = null;
+		comboBoxService.setItems(null);
+		comboBoxProfil.setItems(null);
+		textFieldNom.clear();
+		textFieldPrenom.clear();
+		textFieldAdresse.clear();
+		textFieldTelephone.clear();
+		textFieldEmail.clear();
+
+	}
+
+
 }
 
