cafa8daf0f19ce0a25a775e2f03f7d0872cf15a4
EspoirNorbert <espoirnorbert@gmail.com>
2022-07-19 14:50:10 +0200

    Creation de la l'interface IRekestData et de sa classe d'implementation

---
+++ application/src/com/rekest/feature/impl/RekestData.java
@@ -0,0 +1,85 @@
+package com.rekest.feature.impl;
+
+import com.rekest.feature.IRekestData;
+
+public class RekestData implements IRekestData {
+
+	@Override
+	public void initAdmins() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initEmployes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initGestionnaire() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initChefServices() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initChefDepartement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initDirecteurGeneral() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initDirection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createDefaultAdmin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initDepartement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initRole() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initProduit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initDemande() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
