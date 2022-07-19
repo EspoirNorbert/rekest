fbc8d5614b29487e25947eafdb8e30f2a4ae003e
ccbobs13 <ccb.obs13@gmail.com>
2022-07-19 06:31:24 +0000

    Complete all for create, update, delete and search

--- application/src/com/rekest/controllers/EmployeController.java
+++ application/src/com/rekest/controllers/EmployeController.java
@@ -4,12 +4,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.rekest.entities.employes.Employe;
-import com.rekest.exeptions.DAOException;
 import com.rekest.feature.IFeature;
 import com.rekest.feature.impl.Feature;
 import com.rekest.utils.Utilitaire;
 
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,6 +21,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -76,45 +78,117 @@ public class EmployeController implements Initializable {
 
 	private IFeature feature = Feature.getCurrentInstance();
 
+	private ObservableList<Employe> employees;
+
 	@FXML
 	void handleClickedAjouter(ActionEvent event) {
+		
 		Employe tempEmp = new Employe();
-		boolean okClicked = showEmployeEditDialog(tempEmp);
+		boolean okClicked = showEmployeEditDialog(tempEmp, "Creation d'un employe");
 		if (okClicked) {
 			tempEmp = employeEditDialogController.getEmploye();
 			Boolean statut =  feature.creerEmploye(tempEmp);
 			if (statut) {
-				
+				refreshCount();
+
 			}
 		}
 	}
 
 	@FXML
 	void handleClickedModifier(ActionEvent event) {
+		
+		Employe selectedEmploye = tableViewEmployes.getSelectionModel().getSelectedItem();
+		if (selectedEmploye != null) {
+			boolean okClicked = showEmployeEditDialog(selectedEmploye, "Modification d'un employe");
+			if (okClicked) {
+				Employe tempEmp = employeEditDialogController.getEmploye();
+				Boolean statut =  feature.modifierEmploye(tempEmp);
+				if (statut) {
+					refreshCount();
 
+				}
+			}
+
+		} else {
+			// Nothing selected
+			Utilitaire.alert(AlertType.WARNING, 
+					null, 
+					"No selection",
+					"No Employe selected",
+					"Please select an employe in the table ");
+
+		}
 	}
 
 	@FXML
 	void handleClickedRecherche(ActionEvent event) {
+		
+		// NO NEEDED WHEN USING EVENT ON TEXTFIELD RECHERCHE
 
+//		String whereClause;
+//		if(!textRecherche.getText().isEmpty()) {
+//			System.out.println("EmployeController.handleClickedRecherche()" + textRecherche.getText());
+//			whereClause = "WHERE nom LIKE '%"+textRecherche.getText()+"%'";
+//			System.out.println("EmployeController.handleClickedRecherche()" + whereClause);
+//			List<Employe> employeTrouve = feature.listerEmployes(whereClause);
+//
+//			if(employeTrouve!=null)
+//				System.out.println("EmployeController.handleClickedRecherche()" + employeTrouve.toString());
+//		}
 	}
 
 	@FXML
 	void handleClickedSupprimer(ActionEvent event) {
 
+		int selectedIndex = tableViewEmployes.getSelectionModel().getSelectedIndex();
+		if(selectedIndex >= 0 && tableViewEmployes.getSelectionModel().isSelected(selectedIndex)) {
+			Employe selectedEmploye = tableViewEmployes.getSelectionModel().getSelectedItem();
+
+			Boolean confirmStatus = Utilitaire.confirm(AlertType.CONFIRMATION,
+					null, 
+					"Delete",
+					"Delete \""+selectedEmploye.getNom()+ "\" ? ",
+					"Are you sure you want to delete this employe?");
+
+			if(confirmStatus) {
+//				tableViewEmployes.getItems().remove(selectedIndex);
+				Boolean statut = feature.supprimerEmploye(selectedEmploye);
+				if(statut) {
+					for (Employe employe : tableViewEmployes.getItems()) {
+						System.out.println("EmployeController.handleClickedSupprimer()"
+					+employe.getId() + " " +employe.getNom() +"  "+ employe.getServiceName() + "\n");
+					}
+					refreshCount();
+					
+				}
+			}
+
+
+		} else {
+			// Nothing selected
+			Utilitaire.alert(AlertType.WARNING, 
+					null, 
+					"No selection",
+					"No Employe selected",
+					"Please select an employe in the table ");
+
+		}
 	}
 
-	public boolean showEmployeEditDialog(Employe employe) {
+
+	public boolean showEmployeEditDialog(Employe employe, String title) {
 		try {
 			FXMLLoader loader = Utilitaire.initFXMLoader("EmployeEditDialog");
 			AnchorPane root = (AnchorPane) Utilitaire.loadFXMLFile(loader, false);
-			Stage dialogStage = Utilitaire.createDialog(root, this.primaryStage, "Creation d'un employe");
+			Stage dialogStage = Utilitaire.createDialog(root, this.primaryStage, title);
 
 			// Set the employe into the controller.
 			employeEditDialogController = loader.getController();
 			employeEditDialogController.setDialogStage(dialogStage);
 			employeEditDialogController.setEmploye(employe);
 			employeEditDialogController.setServices();
+			employeEditDialogController.setProfiles();
 
 			Utilitaire.showDialog(dialogStage);
 			return employeEditDialogController.isOkClicked();
@@ -128,15 +202,80 @@ public class EmployeController implements Initializable {
 	}
 
 	public void loadDataInTable() {
-		// Add observable list data to the table
-		ObservableList<Employe> employes = feature.loadEmployesObservableList();
-		tableViewEmployes.setItems(employes);
+		// Init properties
+		initProperties();
 
-		if (employes.size() > 0) tableViewEmployes.getSelectionModel().select(0);
+		// Add observable list data to the table
+		setEmployees(feature.loadEmployesObservableList());
+		tableViewEmployes.setItems(getEmployees());
+
+		if (getEmployees().size() > 0) 
+			tableViewEmployes.getSelectionModel().select(0);
+		
+		refreshCount();
+
+		// Wrap the ObservaleList in a FilteredList (initially display all data)
+		FilteredList<Employe> filteredEmployes = new FilteredList<>(getEmployees(), b -> true);
+
+		// Set the filter Predicate whenever the filter changes
+		textRecherche.textProperty().addListener((observable, oldValue, newValue) -> {
+			
+			filteredEmployes.setPredicate(employe -> {
+				// If filter text is blank or null or empty -> display all employees
+				if(newValue.isBlank() || newValue.isEmpty() || newValue == null) 
+					return true;
+
+				// Compare the firstname and lastname of every employe with keyword
+				String keyword = newValue.toLowerCase();
+
+				if(employe.getNom().toLowerCase().indexOf(keyword) > -1) {
+					return true; // Filter matches lastname
+				}
+				else if(employe.getPrenom().toLowerCase().indexOf(keyword) > -1) {
+					return true; // Filter matches firstname
+				}
+				else {
+					return false; // No matches
+				}
+
+			});
+		});
+
+		// Wrap the FilteredList in a SortedList 
+		SortedList<Employe> sortedEmployes = new SortedList<>(filteredEmployes);
+
+		// Bind the SortedList comparator to the TableView comparator, otherwise sorting will have no effect
+		sortedEmployes.comparatorProperty().bind(tableViewEmployes.comparatorProperty());
+
+		// Add sorted (and filtered) data to the table 
+		tableViewEmployes.setItems(sortedEmployes);
+		
 	}
 
-	public void showServices() throws DAOException {
-		System.out.println("EmployeController.showServices()" + feature.listerServices());
+	public void initProperties(){
+
+		columnNom.setCellValueFactory(new PropertyValueFactory<>("nom"));
+		columnPrenom.setCellValueFactory(new PropertyValueFactory<>("prenom"));
+		columnAdresse.setCellValueFactory(new PropertyValueFactory<>("adresse"));
+		columnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		columnProfil.setCellValueFactory(new PropertyValueFactory<>("employeProfil"));
+		columnTelephone.setCellValueFactory(new PropertyValueFactory<>("telephone"));
+		columnService.setCellValueFactory(new PropertyValueFactory<>("serviceName"));
+
+	}
+
+
+	public ObservableList<Employe> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(ObservableList<Employe> employees) {
+		this.employees = employees;
+	}
+
+	public void refreshCount() {
+		countEmployes.setText(String.valueOf(getEmployees().size()));
+
 	}
 
 	@Override
