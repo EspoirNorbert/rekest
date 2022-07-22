0a67b2311a4d0d6d9bce875378e99b2faf57a88a
ccbobs13 <ccb.obs13@gmail.com>
2022-07-22 22:13:48 +0000

    Remove the implementation of faker

--- application/src/com/rekest/feature/impl/Feature.java
+++ application/src/com/rekest/feature/impl/Feature.java
@@ -3,7 +3,6 @@ package com.rekest.feature.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.github.javafaker.Faker;
 import com.rekest.dao.IDao;
 import com.rekest.dao.impl.HibernateDao;
 import com.rekest.entities.Demande;
@@ -12,10 +11,6 @@ import com.rekest.entities.Note;
 import com.rekest.entities.Produit;
 import com.rekest.entities.Role;
 import com.rekest.entities.Service;
-import com.rekest.entities.employes.Administrateur;
-import com.rekest.entities.employes.ChefService;
-import com.rekest.entities.employes.Directeur;
-import com.rekest.entities.employes.DirecteurGeneral;
 import com.rekest.entities.employes.Employe;
 import com.rekest.entities.employes.Manager;
 import com.rekest.entities.employes.Utilisateur;
@@ -67,124 +62,6 @@ public class Feature implements IFeature {
 		if (instance == null) instance = new Feature ();
 		return instance;
 	}
-
-	private Faker faker = new Faker();
-
-
-	@Override
-	public void initDepartement() {
-		try {
-			for (int i = 0; i <= 10; i++) {
-				Departement department = 
-						new Departement(faker.commerce().department());
-				dao.save(department);
-			}
-		} catch (DAOException e) {
-			System.err.println(e.getMessage());
-		}
-	}
-
-	@Override
-	public void initEmploye() {
-		try {
-			for (int i = 0; i <= 10; i++) {
-				Employe employe = new Employe(
-						faker.name().lastName(),
-						faker.name().firstName(),
-						faker.phoneNumber().cellPhone(), faker.internet().emailAddress(), faker.address().fullAddress());
-				dao.save(employe);
-			}
-		} catch (DAOException e) {
-			System.err.println(e.getMessage());
-		}
-	}
-
-	@Override
-	public void initAdmin() {
-		try {
-			dao.save(createDefaultAdmin());
-			for (int i = 0; i <= 3; i++) {
-				Administrateur admin = new Administrateur(
-						faker.name().lastName(),
-						faker.name().firstName(),
-						faker.phoneNumber().cellPhone(), faker.internet().emailAddress(), faker.address().fullAddress());
-
-				dao.save(admin);
-			}
-		} catch (DAOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void initManagers() {
-		try {
-			// List<Manager> managers = new ArrayList<>();
-			for (int i = 0; i <= 10; i++) {
-				ChefService chefService = new ChefService(faker.name().firstName(), faker.name().lastName(),
-						faker.phoneNumber().cellPhone(), faker.internet().emailAddress(), faker.address().fullAddress());
-				Utilitaire.generateLoginAndPassword(chefService);
-				dao.save(chefService);
-				Directeur directeur = new Directeur(faker.name().firstName(), faker.name().lastName(),
-						faker.phoneNumber().cellPhone(), faker.internet().emailAddress(), faker.address().fullAddress());
-				Utilitaire.generateLoginAndPassword(directeur);
-				dao.save(directeur);
-				DirecteurGeneral directeurGeneral = new DirecteurGeneral(faker.name().firstName(),
-						faker.name().lastName(), faker.phoneNumber().cellPhone(), faker.internet().emailAddress(),
-						faker.address().fullAddress());
-				Utilitaire.generateLoginAndPassword(directeurGeneral);
-				dao.save(directeur);
-			}
-		} catch (DAOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public Administrateur createDefaultAdmin() {
-		Administrateur admin =  new Administrateur("Administrator", "System", "+221771234500", "rekest.app@rekest.sn",
-				"Terrain foyer Rocade Fann Bel Air, BP 10 000 Dakar Liberté – SENEGAL");
-		admin.setLogin("admin");
-		admin.setPassword("admin");
-		return admin;
-	}
-
-	@Override
-	public void initService() {
-		try {
-			for (int i = 0; i <= 10; i++) {
-				ChefService chefService = new ChefService(faker.name().firstName(), faker.name().lastName(),
-						faker.phoneNumber().cellPhone(), faker.internet().emailAddress(), faker.address().fullAddress());	
-				Utilitaire.generateLoginAndPassword(chefService);
-				dao.save(chefService);
-
-				Departement department = 
-						new Departement(faker.commerce().department());
-
-				Service service = new Service(faker.commerce().department());
-
-				service.setChefService(chefService);
-				dao.save(service);
-
-				department.addService(service);
-				dao.save(department);
-
-			}
-		} catch (DAOException e) {
-			System.err.println(e.getMessage());
-		}
-	}
-
-
-	@Override
-	public void initAllEntity() {
-		this.initAdmin();
-		this.initEmploye();
-		this.initManagers();
-		this.initDepartement();
-		this.initService();
-	}
-
 
 	public ObservableListDemande getObservableListDemande() {
 		return observableListDemande;
