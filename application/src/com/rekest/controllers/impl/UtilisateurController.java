8aaeb497ce7058cab55ff25ad61bba93df08c3bb
ccb <ccb@mbp-de-samyr>
2022-07-24 23:12:05 +0000

    Add handleClickedRecherche method to suppress some warnings

--- application/src/com/rekest/controllers/impl/UtilisateurController.java
+++ application/src/com/rekest/controllers/impl/UtilisateurController.java
@@ -1,5 +1,10 @@
 package com.rekest.controllers.impl;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class UtilisateurController {
 
+	@FXML
+	void handleClickedRecherche(ActionEvent event) {}
 }
