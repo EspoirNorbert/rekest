c8df8be8ff924465438631cb51134c1913c538a8
EspoirNorbert <espoirnorbert@gmail.com>
2022-07-24 14:38:10 +0100

    Suppression de l'important des streams dans l'inteface IFeature

--- application/src/com/rekest/feature/impl/Feature.java
+++ application/src/com/rekest/feature/impl/Feature.java
@@ -2,8 +2,6 @@ package com.rekest.feature.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Stream;
 
 import com.rekest.dao.IDao;
 import com.rekest.dao.impl.HibernateDao;
